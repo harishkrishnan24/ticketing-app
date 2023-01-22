0922079f20746b4bc91adacf07ee7beef1259cde
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-01-22 20:59:30 +0530

    Add custom error classes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
@@ -16,10 +18,10 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
-    const { email, password } = req.body;
+    throw new DatabaseConnectionError();
 
     res.send({});
   }
