6558eb420cbf2ab3cb4445770790b4214af5cf3b
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-02-04 17:19:07 +0530

    Add custom error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,13 +7,9 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    console.log("handling this error as a request validation error");
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  if (err instanceof DatabaseConnectionError) {
-    console.log("handling this error as a db connection error");
-  }
-
-  res.status(400).send({ message: err.message });
+  res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
