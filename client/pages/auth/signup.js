1db1a39bcced96a0210183bc33ab82a65cc9ef7f
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-02 17:02:47 +0530

    signup page

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    onSuccess: () => {
+      Router.push("/");
+    },
+    body: { email, password },
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign up</h1>
+      <div className='form-group'>
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setEmail(e.target.value)}
+          type='password'
+          className='form-control'
+        />
+      </div>
+      {errors}
+      <button className='btn btn-primary'>Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
