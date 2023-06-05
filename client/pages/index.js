1a84b6583032abccdc9c42db657dca2a686137cb
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-05 20:04:05 +0530

    fix ssr cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,13 @@
-const LandingPage = () => {
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
