37c7d4926d574842a833586b2c0a6d02bcf1c6a7
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-05 21:34:29 +0530

    signout functionality

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
-const Signup = () => {
+const Signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { doRequest, errors } = useRequest({
-    url: "/api/users/signup",
+    url: "/api/users/signin",
     method: "post",
     onSuccess: () => {
       Router.push("/");
@@ -21,7 +21,7 @@ const Signup = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h1>Sign up</h1>
+      <h1>Sign In</h1>
       <div className='form-group'>
         <label>Email Address</label>
         <input
@@ -40,9 +40,9 @@ const Signup = () => {
         />
       </div>
       {errors}
-      <button className='btn btn-primary'>Sign Up</button>
+      <button className='btn btn-primary'>Sign In</button>
     </form>
   );
 };
 
-export default Signup;
+export default Signin;
---
+++ client/pages/auth/signout.js
@@ -0,0 +1,19 @@
+import Router from "next/router";
+import { useEffect } from "react";
+
+const Signout = () => {
+  const { doRequest } = useRequest({
+    url: "/api/users/signout",
+    method: "post",
+    body: {},
+    onSuccess: () => Router.push("/"),
+  });
+
+  useEffect(() => {
+    doRequest();
+  }, []);
+
+  return <div>Signing you out...</div>;
+};
+
+export default Signout;
