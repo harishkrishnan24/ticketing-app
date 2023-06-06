910269857702f4152e30349bfbacc8006fbf5fa0
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-06 20:34:09 +0530

    make use of common module

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@harishkrishnan1993/common";
 
 const router = express.Router();
 
