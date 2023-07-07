b2c5c8bda7381899e6448007018f3b61d064f5af
Harish Krishnan <harishkrishnan@pop-os.localdomain>
2023-07-07 18:08:50 +0530

    add routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -8,10 +8,10 @@ import {
   errorHandler,
   currentUser,
 } from "@harishkrishnan1993/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { indexOrderRouter } from "./routes";
+import { showOrderRouter } from "./routes/show";
+import { newOrderRouter } from "./routes/new";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 app.set("trust proxy", true);
@@ -24,10 +24,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(indexOrderRouter);
+app.use(showOrderRouter);
+app.use(newOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
