bcc3d42534c650b4ec9224b66daf853dd6618766
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-07-10 16:00:07 +0530

    orders service API endpoints

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,53 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it("returns an error if the ticket does not exist", async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it("returns an error if the ticket is already reserved", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    userId: "asdsad",
+    ticket,
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it("reserves a ticket", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
+
+it.todo("emits an order created event");
