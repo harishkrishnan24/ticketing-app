21679fe7d9e0f57fbee11e5d7dd5f1fda2a14a45
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-07-29 15:02:37 +0530

    skaffold for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,29 @@
+import "express-async-errors";
+
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import express from "express";
+import {
+  NotFoundError,
+  errorHandler,
+  currentUser,
+} from "@harishkrishnan1993/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
